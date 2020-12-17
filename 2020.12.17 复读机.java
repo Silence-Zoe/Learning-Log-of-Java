0fef088ce32ebc0151ebfc0cd7b2d72d0aaf1b1f
Silence <72530910+Silence-Zoe@users.noreply.github.com>
2020-12-17 19:10:28 +0800

    Create 2020.12.17 复读机.java

---
+++ 2020.12.17 复读机.java
@@ -0,0 +1,13 @@
+package test;
+
+import java.util.Scanner;
+
+public class Test {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner in = new Scanner(System.in);
+		System.out.println(in.nextLine());
+	}
+
+}

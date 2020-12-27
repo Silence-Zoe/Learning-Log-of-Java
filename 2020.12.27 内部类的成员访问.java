b510725100eebe60621983b4047260171eb36769
Silence <72530910+Silence-Zoe@users.noreply.github.com>
2020-12-27 08:09:13 +0800

    Create 2020.12.27 内部类的成员访问.java

---
+++ 2020.12.27 内部类的成员访问.java
@@ -0,0 +1,24 @@
+package test;
+
+
+public class Test {
+	
+	public static void main(String[] args) {
+		Outer.Inner oi = new Outer().new Inner();
+		oi.show();
+	}
+	
+}
+
+class Outer {
+	public int num = 10;
+	class Inner {
+		public int num = 20;
+		public void show() {
+			int num = 30;
+			System.out.println(num);			//30
+			System.out.println(this.num);		//20
+			System.out.println(Outer.this.num);	//10
+		}
+	}
+}

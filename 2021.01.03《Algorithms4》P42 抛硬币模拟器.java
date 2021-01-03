1e4d61b529304024413c1f45fbe4519375c02447
Silence <72530910+Silence-Zoe@users.noreply.github.com>
2021-01-03 15:00:28 +0800

    Create 2021.01.03《Algorithms4》P42 抛硬币模拟器.java

---
+++ 2021.01.03《Algorithms4》P42 抛硬币模拟器.java
@@ -0,0 +1,24 @@
+package test;
+
+import java.util.Scanner;
+import edu.princeton.cs.algs4.*;
+
+public class Flips {
+	public static void main(String[] args) {
+		Scanner in = new Scanner(System.in);
+		int T = in.nextInt();
+		Counter heads = new Counter("heads");
+		Counter tails = new Counter("tails");
+		for (int t = 0; t < T; t++) {
+			if (StdRandom.bernoulli(0.5)) {
+				heads.increment();	
+			} else {
+				tails.increment();
+			}
+		}
+		StdOut.println(heads);
+		StdOut.println(tails);
+		int d = heads.tally() - tails.tally();
+		StdOut.println("delta:" + Math.abs(d));	
+	}
+}

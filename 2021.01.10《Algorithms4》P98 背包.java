1a2a00ba88098310a1019893127d44368820f397
Silence <72530910+Silence-Zoe@users.noreply.github.com>
2021-01-10 17:30:49 +0800

    Create 2021.01.10《Algorithms4》P98 背包.java

---
+++ 2021.01.10《Algorithms4》P98 背包.java
@@ -0,0 +1,43 @@
+package test;
+
+import java.util.Iterator;
+
+public class Bag<Item> implements Iterable<Item> {
+	private Node first;
+	
+	private class Node {
+		Item item;
+		Node next;
+	}
+	
+	public boolean isEmpty() {
+		return first == null;
+	}
+
+	public void add(Item item) {
+		Node oldfirst = first;
+		first = new Node();
+		first.item = item;
+		first.next = oldfirst;
+	}
+	
+	public Iterator<Item> iterator(){
+		return new ListIterator();
+	}
+	
+	public class ListIterator implements Iterator<Item>{
+		private Node current = first;
+		public boolean hasNext() {
+			return current != null;
+		}
+		
+		public void remove() {}
+		
+		public Item next() {
+			Item item = current.item;
+			current = current.next;
+			return item;
+		}
+	}
+}
+

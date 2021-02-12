37fd9ad41380b5be915ddefb1d52015c4c450e96
Silence <72530910+Silence-Zoe@users.noreply.github.com>
2021-02-12 16:56:06 +0800

    Update 2021.02.12 LC283.移动零.java

--- 2021.02.12 LC283.移动零.java
+++ 2021.02.12 LC283.移动零.java
@@ -1,3 +1,9 @@
+/**
+ * 执行用时：7 ms
+ * 在所有 Java 提交中击败了 8.80% 的用户
+ * 内存消耗：38.8 MB
+ * 在所有 Java 提交中击败了 48.70% 的用户
+ */
 class Solution {
     public void moveZeroes(int[] nums) {
         int cnt = 0;

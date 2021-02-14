61dfdaf2d5375dfa1eadc92e9081088a2766eb55
Silence <72530910+Silence-Zoe@users.noreply.github.com>
2021-02-14 16:15:19 +0800

    Create 2021.02.14 LC075.颜色分类[双指针]（官方）.java

---
+++ 2021.02.14 LC075.颜色分类[双指针]（官方）.java
@@ -0,0 +1,29 @@
+/**
+ * 执行用时：0 ms，在所有 Java 提交中击败了 100.00% 的用户
+ * 内存消耗：37.1 MB，在所有 Java 提交中击败了 30.65% 的用户
+ */
+class Solution {
+    public void sortColors(int[] nums) {
+        int n = nums.length;
+        int p0 = 0, p1 = 0;
+        for (int i = 0; i < n; i++) {
+            if (nums[i] == 1) {
+                int temp = nums[i];
+                nums[i] = nums[p1];
+                nums[p1] = temp;
+                p1++;
+            } else if (nums[i] == 0) {
+                int temp = nums[i];
+                nums[i] = nums[p0];
+                nums[p0] = temp;
+                if (p0 < p1) {
+                    temp = nums[i];
+                    nums[i] = nums[p1];
+                    nums[p1] = temp;
+                }
+                p0++;
+                p1++;
+            }
+        }
+    }
+}

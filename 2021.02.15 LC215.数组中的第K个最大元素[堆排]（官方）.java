e5353751c9674c6138abfcca1590f24b4c7376dd
Silence <72530910+Silence-Zoe@users.noreply.github.com>
2021-02-15 10:37:40 +0800

    Create 2021.02.15 LC215.数组中的第K个最大元素[堆排]（官方）.java

---
+++ 2021.02.15 LC215.数组中的第K个最大元素[堆排]（官方）.java
@@ -0,0 +1,42 @@
+/**
+ * 执行用时：2 ms，在所有 Java 提交中击败了 90.27% 的用户
+ * 内存消耗：38.6 MB，在所有 Java 提交中击败了 83.97% 的用户
+ */
+class Solution {
+    public int findKthLargest(int[] nums, int k) {
+        int heapSize = nums.length;
+        buildMaxHeap(nums, heapSize);
+        for (int i = nums.length - 1; i >= nums.length - k + 1; i--) {
+            swap(nums, 0, i);
+            heapSize--;
+            maxHeapify(nums, 0, heapSize);
+        }
+        return nums[0];
+    }
+
+    public void buildMaxHeap(int[] a, int heapSize) {
+        for (int i = heapSize / 2; i >= 0; i--) {
+            maxHeapify(a, i, heapSize);
+        } 
+    }
+
+    public void maxHeapify(int[] a, int i, int heapSize) {
+        int l = i * 2 + 1, r = i * 2 + 2, largest = i;
+        if (l < heapSize && a[l] > a[largest]) {
+            largest = l;
+        } 
+        if (r < heapSize && a[r] > a[largest]) {
+            largest = r;
+        }
+        if (largest != i) {
+            swap(a, i, largest);
+            maxHeapify(a, largest, heapSize);
+        }
+    }
+
+    public void swap(int[] a, int i, int j) {
+        int temp = a[i];
+        a[i] = a[j];
+        a[j] = temp;
+    }
+}

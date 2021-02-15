9928428069940dc988c843728eed6784a45cc15b
Silence <72530910+Silence-Zoe@users.noreply.github.com>
2021-02-15 10:34:52 +0800

    Create 2021.02.15 LC215.数组中的第K个最大元素[快排]（官方）.java

---
+++ 2021.02.15 LC215.数组中的第K个最大元素[快排]（官方）.java
@@ -0,0 +1,43 @@
+/**
+ * 执行用时：2 ms，在所有 Java 提交中击败了 90.27% 的用户
+ * 内存消耗：38.7 MB，在所有 Java 提交中击败了 67.62% 的用户
+ */
+class Solution {
+    Random random = new Random();
+    
+    public int findKthLargest(int[] nums, int k) {
+        return quickSelect(nums, 0, nums.length - 1, nums.length - k);
+    }
+
+    public int quickSelect(int[] a, int l, int r, int index) {
+        int q = randomPartition(a, l, r);
+        if (q == index) {
+            return a[q];
+        } else {
+            return q < index ? quickSelect(a, q + 1, r, index) : quickSelect(a, l, q - 1, index);  
+        }
+    }
+
+    public int randomPartition(int[] a, int l, int r) {
+        int i = random.nextInt(r - l + 1) + l;
+        swap(a, i, r);
+        return partition(a, l, r);
+    }
+
+    public int partition(int[] a, int l, int r) {
+        int x = a[r], i = l - 1;
+        for (int j = l; j < r; j++) {
+            if (a[j] <= x) {
+                swap(a, ++i, j);
+            }
+        }
+        swap(a, i + 1, r);
+        return i + 1;
+    }
+
+    public void swap(int[] a, int i, int j) {
+        int temp = a[i];
+        a[i] = a[j];
+        a[j] = temp;
+    }
+}

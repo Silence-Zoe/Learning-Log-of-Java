90b42e7badda6ce3558041f9a1ee2b83c519cb6b
Silence <72530910+Silence-Zoe@users.noreply.github.com>
2021-02-16 09:30:18 +0800

    Create 2021.02.16 LC088.合并两个有序数组.java

---
+++ 2021.02.16 LC088.合并两个有序数组.java
@@ -0,0 +1,24 @@
+/**
+ * 执行用时：0 ms，在所有 Java 提交中击败了 100.00% 的用户
+ * 内存消耗：38.3 MB，在所有 Java 提交中击败了 92.34% 的用户
+ */
+class Solution {
+    public void merge(int[] nums1, int m, int[] nums2, int n) {
+        int p = nums1.length - nums2.length - 1, q = nums2.length - 1;
+        for (int i = nums1.length - 1; i >= 0; i--) {
+            if (nums2.length == 0) {
+                return;
+            }
+            if (nums1.length == nums2.length) {
+                for (int j = 0; j < nums1.length; j++) {
+                    nums1[j] = nums2[j];
+                }
+            }
+            if (p >= 0 && q >= 0) {
+                nums1[i] = nums1[p] > nums2[q] ? nums1[p--] : nums2[q--];
+            } else {
+                nums1[i] = p == -1 ? nums2[q--] : nums1[p--];
+            }
+        }
+    }
+}

67c56fcbe907eef8490a4fbf9b215dbeacedaba1
Silence <72530910+Silence-Zoe@users.noreply.github.com>
2021-02-17 15:16:49 +0800

    Create 2021.02.17 LC167.两数之和Ⅱ - 输入有序数组[二分]（官方）.java

---
+++ 2021.02.17 LC167.两数之和Ⅱ - 输入有序数组[二分]（官方）.java
@@ -0,0 +1,34 @@
+/**
+ * 执行用时：4 ms，在所有 Java 提交中击败了 23.86% 的用户
+ * 内存消耗：38.7 MB，在所有 Java 提交中击败了 50.25% 的用户
+ */
+class Solution {
+    public int[] twoSum(int[] numbers, int target) {
+        for (int i = 0; i < numbers.length; i++) {
+            int low = i + 1, high = numbers.length - 1;
+            while (low <= high) {
+                int mid = (high - low) / 2 + low;
+                if (numbers[mid] == target - numbers[i]) {
+                    return new int[]{i + 1, mid + 1};
+                } else if (numbers[mid] > target - numbers[i]) {
+                    high = mid - 1;
+                } else{
+                    low = mid + 1;
+                }
+            }
+        }
+        return new int[]{-1, -1};
+    }
+}
+/**
+ * 给定一个已按照 升序排列 的整数数组 numbers ，请你从数组中找出两个数满足相加之和等于目标数 target 。
+ * 函数应该以长度为 2 的整数数组的形式返回这两个数的下标值。numbers 的下标 从 1 开始计数 ，所以答案数组应当满足 1 <= answer[0] < answer[1] <= numbers.length 。
+ * 你可以假设每个输入只对应唯一的答案，而且你不可以重复使用相同的元素。
+ * --------------------------------------------------
+ * 提示：
+ * 2 <= numbers.length <= 3 * 104
+ * -1000 <= numbers[i] <= 1000
+ * numbers 按 递增顺序 排列
+ * -1000 <= target <= 1000
+ * 仅存在一个有效答案
+ */

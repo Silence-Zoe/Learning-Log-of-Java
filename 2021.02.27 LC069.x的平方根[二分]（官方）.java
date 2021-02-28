41e0a62ece3f77e11778a75b2ed920f60b46bf11
Silence <72530910+Silence-Zoe@users.noreply.github.com>
2021-02-28 09:12:20 +0800

    Create 2021.02.27 LC069.x的平方根[二分]（官方）.java

---
+++ 2021.02.27 LC069.x的平方根[二分]（官方）.java
@@ -0,0 +1,24 @@
+/**
+ * 执行用时：2 ms，在所有 Java 提交中击败了 47.12% 的用户
+ * 内存消耗：35.8 MB，在所有 Java 提交中击败了 9.12% 的用户
+ */
+class Solution {
+    public int mySqrt(int x) {
+        int l = 0, r = x, ans = -1;
+        while (l <= r) {
+            int mid = l + (r - l) / 2;
+            if ((long)mid * mid <= x) {
+                ans = mid;
+                l = mid + 1;
+            } else {
+                r = mid - 1;
+            }
+        }
+        return ans;
+    }
+}
+/**
+ * 实现 int sqrt(int x) 函数。
+ * 计算并返回 x 的平方根，其中 x 是非负整数。
+ * 由于返回类型是整数，结果只保留整数的部分，小数部分将被舍去。
+ */

e747831e19a4dbb981c80a57d2abcca8d805bce0
Silence <72530910+Silence-Zoe@users.noreply.github.com>
2021-03-02 19:46:15 +0800

    Create 2021.03.02 LC278.第一个错误的版本（官方）.java

---
+++ 2021.03.02 LC278.第一个错误的版本（官方）.java
@@ -0,0 +1,23 @@
+/**
+ * 执行用时：16 ms，在所有 Java 提交中击败了 99.65% 的用户
+ * 内存消耗：35.3 MB，在所有 Java 提交中击败了 28.49% 的用户
+ */
+public class Solution extends VersionControl {
+    public int firstBadVersion(int n) {
+        int left = 1;
+        int right = n;
+        while (left < right) {
+             int mid = left + (right - left) / 2;
+             if (isBadVersion(mid)) {
+                 right = mid;
+             } else {
+                 left = mid + 1;
+             }
+        }
+    return left;
+    }
+}/**
+ * 你是产品经理，目前正在带领一个团队开发新的产品。不幸的是，你的产品的最新版本没有通过质量检测。由于每个版本都是基于之前的版本开发的，所以错误的版本之后的所有版本都是错的。
+ * 假设你有 n 个版本 [1, 2, ..., n]，你想找出导致之后所有版本出错的第一个错误的版本。
+ * 你可以通过调用 bool isBadVersion(version) 接口来判断版本号 version 是否在单元测试中出错。实现一个函数来查找第一个错误的版本。你应该尽量减少对调用 API 的次数。
+ */

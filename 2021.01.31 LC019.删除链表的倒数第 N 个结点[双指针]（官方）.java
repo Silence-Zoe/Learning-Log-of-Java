3239d12b913fda2213888511376b36c8d1ed9d95
Silence <72530910+Silence-Zoe@users.noreply.github.com>
2021-03-02 19:59:23 +0800

    Update 2021.01.31 LC019.删除链表的倒数第 N 个结点[双指针]（官方）.java

--- 2021.01.31 LC019.删除链表的倒数第 N 个结点[双指针]（官方）.java
+++ 2021.01.31 LC019.删除链表的倒数第 N 个结点[双指针]（官方）.java
@@ -1,3 +1,7 @@
+/**
+ * 执行用时：0 ms，在所有 Java 提交中击败了 100.00% 的用户
+ * 内存消耗：36.4 MB，在所有 Java 提交中击败了 58.19% 的用户
+ */
 class Solution {
     public ListNode removeNthFromEnd(ListNode head, int n) {
       ListNode dummy = new ListNode(0, head);
@@ -15,3 +19,12 @@ class Solution {
       return ans;
     }
 }
+/**
+ * 给你一个链表，删除链表的倒数第 n 个结点，并且返回链表的头结点。
+ * --------------------------------------------------
+ * 提示：
+ * 链表中结点的数目为 sz
+ * 1 <= sz <= 30
+ * 0 <= Node.val <= 100
+ * 1 <= n <= sz
+ */

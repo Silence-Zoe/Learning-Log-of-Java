da5681905f3b6b99943f3d8704c80c5ce57df896
Silence <72530910+Silence-Zoe@users.noreply.github.com>
2021-03-02 19:50:45 +0800

    Update 2021.01.30 LC160.相交链表[双指针].java

--- 2021.01.30 LC160.相交链表[双指针].java
+++ 2021.01.30 LC160.相交链表[双指针].java
@@ -1,13 +1,6 @@
 /**
- * Definition for singly-linked list.
- * public class ListNode {
- *     int val;
- *     ListNode next;
- *     ListNode(int x) {
- *         val = x;
- *         next = null;
- *     }
- * }
+ * 执行用时：1 ms，在所有 Java 提交中击败了 100.00% 的用户
+ * 内存消耗：41 MB，在所有 Java 提交中击败了 88.47% 的用户
  */
 public class Solution {
     public ListNode getIntersectionNode(ListNode headA, ListNode headB) {
@@ -50,3 +43,12 @@ public class Solution {
         return slow;
     }
 }
+/**
+ * 编写一个程序，找到两个单链表相交的起始节点。
+ * --------------------------------------------------
+ * 提示：
+ * 如果两个链表没有交点，返回 null.
+ * 在返回结果后，两个链表仍须保持原有的结构。
+ * 可假定整个链表结构中没有循环。
+ * 程序尽量满足 O(n) 时间复杂度，且仅用 O(1) 内存。
+ */

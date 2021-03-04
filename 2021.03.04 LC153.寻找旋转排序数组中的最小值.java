ce32edf2c6567aa2390d4d0a9bc98c953cfdabc1
Silence <72530910+Silence-Zoe@users.noreply.github.com>
2021-03-04 15:02:31 +0800

    Create 2021.03.04 LC153.寻找旋转排序数组中的最小值.java

---
+++ 2021.03.04 LC153.寻找旋转排序数组中的最小值.java
@@ -0,0 +1,32 @@
+/**
+ * 执行用时：0 ms，在所有 Java 提交中击败了 100.00% 的用户
+ * 内存消耗：38 MB，在所有 Java 提交中击败了 25.83% 的用户
+ */
+class Solution {
+    public int findMin(int[] nums) {
+        int n = nums.length;
+        if (n == 1) {
+            return nums[0];
+        }
+        int left = 0, right = n - 1;
+        while (left < right) {
+            int m = left + (right - left) / 2;
+            if (nums[left] <= nums[m] && nums[left] > nums[right]) {
+                left = m + 1;
+            } else {
+                right = m;
+            }
+        }
+        return nums[left];
+    }
+}
+/**
+ * 假设按照升序排序的数组在预先未知的某个点上进行了旋转。例如，数组 [0,1,2,4,5,6,7] 可能变为 [4,5,6,7,0,1,2] 。
+ * 请找出其中最小的元素。
+ * --------------------------------------------------
+ * 提示：
+ * nums 中的所有整数都是 唯一 的
+ * nums 原来是一个升序排序的数组，但在预先未知的某个点上进行了旋转
+ * -5000 <= nums[i] <= 5000
+ * 1 <= nums.length <= 5000
+ */

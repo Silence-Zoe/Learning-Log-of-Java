de4c03fc16fd632e7ef6bf61ffeecba63cc99e07
Silence <72530910+Silence-Zoe@users.noreply.github.com>
2021-03-13 15:47:43 +0800

    Create 2021.03.13 LC350.两个数组的交集Ⅱ[双指针]（官方）.java

---
+++ 2021.03.13 LC350.两个数组的交集Ⅱ[双指针]（官方）.java
@@ -0,0 +1,33 @@
+/**
+ * 执行用时：1 ms，在所有 Java 提交中击败了 100.00% 的用户
+ * 内存消耗：38.8 MB，在所有 Java 提交中击败了 13.44% 的用户
+ */
+class Solution {
+    public int[] intersect(int[] nums1, int[] nums2) {
+        Arrays.sort(nums1);
+        Arrays.sort(nums2);
+        int length1 = nums1.length, length2 = nums2.length;
+        int[] intersection = new int[Math.min(length1, length2)];
+        int index1 = 0, index2 = 0, index = 0;
+        while (index1 < length1 && index2 < length2) {
+            if (nums1[index1] < nums2[index2]) {
+                index1++;
+            } else if (nums1[index1] > nums2[index2]) {
+                index2++;
+            } else {
+                intersection[index] = nums1[index1];
+                index1++;
+                index2++;
+                index++;
+            }
+        }
+        return Arrays.copyOfRange(intersection, 0, index);
+    }
+}
+/**
+ * 给定两个数组，编写一个函数来计算它们的交集。
+ * --------------------------------------------------
+ * 提示：
+ * 输出结果中每个元素出现的次数，应与元素在两个数组中出现次数的最小值一致。
+ * 我们可以不考虑输出结果的顺序。
+ */

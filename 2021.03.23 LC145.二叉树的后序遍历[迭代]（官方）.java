7830705a2dd41f8fe91e63865802cc085ca440fa
Silence <72530910+Silence-Zoe@users.noreply.github.com>
2021-03-23 14:15:15 +0800

    Create 2021.03.23 LC145.二叉树的后序遍历[迭代]（官方）.java

---
+++ 2021.03.23 LC145.二叉树的后序遍历[迭代]（官方）.java
@@ -0,0 +1,38 @@
+/**
+ * 执行用时：1 ms，在所有 Java 提交中击败了 54.01% 的用户
+ * 内存消耗：36.8 MB，在所有 Java 提交中击败了 36.51% 的用户
+ */
+class Solution {
+    public List<Integer> postorderTraversal(TreeNode root) {
+        List<Integer> res = new ArrayList<Integer>();
+        if (root == null) {
+            return res;
+        }
+
+        Deque<TreeNode> stack = new LinkedList<TreeNode>();
+        TreeNode prev = null;
+        while (root != null || !stack.isEmpty()) {
+            while (root != null) {
+                stack.push(root);
+                root = root.left;
+            }
+            root = stack.pop();
+            if (root.right == null || root.right == prev) {
+                res.add(root.val);
+                prev = root;
+                root = null;
+            } else {
+                stack.push(root);
+                root = root.right;
+            }
+        }
+        return res;
+    }
+}
+/**
+ * 给定一个二叉树 ，返回它的 后序 遍历。
+ * --------------------------------------------------
+ * 提示：
+ * 树中节点数目在范围 [0, 100] 内
+ * -100 <= Node.val <= 100
+ */

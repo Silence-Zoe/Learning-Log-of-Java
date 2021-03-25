89e58ca756f5a92e70f50d5a76080f95620686e1
Silence <72530910+Silence-Zoe@users.noreply.github.com>
2021-03-25 18:43:31 +0800

    Create 2021.03.25 LC118.杨辉三角（官方）.java

---
+++ 2021.03.25 LC118.杨辉三角（官方）.java
@@ -0,0 +1,25 @@
+/**
+ * 执行用时：0 ms，在所有 Java 提交中击败了 100.00% 的用户
+ * 内存消耗：36.1 MB，在所有 Java 提交中击败了 97.22% 的用户
+ */
+class Solution {
+    public List<List<Integer>> generate(int numRows) {
+        List<List<Integer>> ret = new ArrayList<List<Integer>>();
+        for (int i = 0; i < numRows; ++i) {
+            List<Integer> row = new ArrayList<Integer>();
+            for (int j = 0; j <= i; ++j) {
+                if (j == 0 || j == i) {
+                    row.add(1);
+                } else {
+                    row.add(ret.get(i - 1).get(j - 1) + ret.get(i - 1).get(j));
+                }
+            }
+            ret.add(row);
+        }
+        return ret;
+    }
+}
+/**
+ * 给定一个非负整数 numRows，生成杨辉三角的前 numRows 行。
+ * 在杨辉三角中，每个数是它左上方和右上方的数的和。
+ */

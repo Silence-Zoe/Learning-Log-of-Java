c60266eab61c2fddf182ef44750806af6784982c
Silence <72530910+Silence-Zoe@users.noreply.github.com>
2021-03-26 18:54:07 +0800

    Create 2021.03.26 LC82.删除排序链表中的重复元素Ⅱ（官方）.java

---
+++ 2021.03.26 LC82.删除排序链表中的重复元素Ⅱ（官方）.java
@@ -0,0 +1,36 @@
+/**
+ * 执行用时：1 ms，在所有 Java 提交中击败了 64.49% 的用户
+ * 内存消耗：37.7 MB，在所有 Java 提交中击败了 76.15% 的用户
+ */
+class Solution {
+    public ListNode deleteDuplicates(ListNode head) {
+        if (head == null) {
+            return head;
+        }
+        
+        ListNode dummy = new ListNode(0, head);
+
+        ListNode cur = dummy;
+        while (cur.next != null && cur.next.next != null) {
+            if (cur.next.val == cur.next.next.val) {
+                int x = cur.next.val;
+                while (cur.next != null && cur.next.val == x) {
+                    cur.next = cur.next.next;
+                }
+            } else {
+                cur = cur.next;
+            }
+        }
+
+        return dummy.next;
+    }
+}
+/**
+ * 存在一个按升序排列的链表，给你这个链表的头节点 head ，请你删除链表中所有存在数字重复情况的节点，只保留原始链表中 没有重复出现 的数字。
+ * 返回同样按升序排列的结果链表。
+ * --------------------------------------------------
+ * 提示：
+ * 链表中节点数目在范围 [0, 300] 内
+ * -100 <= Node.val <= 100
+ * 题目数据保证链表已经按升序排列
+ */

414c39ff0a5555670f06d1a29c2b8f1508ff088b
Silence <72530910+Silence-Zoe@users.noreply.github.com>
2021-04-07 15:28:54 +0800

    Create 2021.04.07 LC852.山脉数组的峰顶索引.java

---
+++ 2021.04.07 LC852.山脉数组的峰顶索引.java
@@ -0,0 +1,31 @@
+/**
+ * 执行用时：0 ms，在所有 Java 提交中击败了 100.00% 的用户
+ * 内存消耗：39 MB，在所有 Java 提交中击败了 7.56% 的用户
+ */
+class Solution {
+    public int peakIndexInMountainArray(int[] arr) {
+        int l = 0, r = arr.length - 1;
+        while (l < r) {
+            int m = l + (r - l) / 2;
+            if (arr[m] < arr[m + 1]) {
+                l = m + 1;
+            } else {
+                r = m;
+            }
+        } 
+        return r;
+    }
+}
+/**
+ * 符合下列属性的数组 arr 称为 山脉数组 ：
+ *     arr.length >= 3
+ *     存在 i（0 < i < arr.length - 1）使得：
+ *         arr[0] < arr[1] < ... arr[i-1] < arr[i]
+ *         arr[i] > arr[i+1] > ... > arr[arr.length - 1]
+ * 给你由整数组成的山脉数组 arr ，返回任何满足 arr[0] < arr[1] < ... arr[i - 1] < arr[i] > arr[i + 1] > ... > arr[arr.length - 1] 的下标 i 。
+ * --------------------------------------------------
+ * 提示：
+ * 3 <= arr.length <= 104
+ * 0 <= arr[i] <= 106
+ * 题目数据保证 arr 是一个山脉数组
+ */

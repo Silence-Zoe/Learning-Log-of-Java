969a39bb94d2fd95e7e8d5c1c5356df82e69f2af
Silence <72530910+Silence-Zoe@users.noreply.github.com>
2021-04-10 14:24:17 +0800

    Create 2021.04.09 LC236.二叉树的最近公共祖先 - 副本.java

---
+++ 2021.04.09 LC236.二叉树的最近公共祖先（官方）.java
@@ -0,0 +1,37 @@
+/**
+ * 执行用时：7 ms，在所有 Java 提交中击败了 99.91% 的用户
+ * 内存消耗：40.6 MB，在所有 Java 提交中击败了 55.97% 的用户
+ */
+class Solution {
+    private TreeNode ans;
+
+    public Solution() {
+        this.ans = null;
+    }
+
+    private boolean dfs(TreeNode root, TreeNode p, TreeNode q) {
+        if (root == null) return false;
+        boolean lson = dfs(root.left, p, q);
+        boolean rson = dfs(root.right, p, q);
+        if ((lson && rson) || ((root.val == p.val || root.val == q.val) && (lson || rson))) {
+            ans = root;
+        } 
+        return lson || rson || (root.val == p.val || root.val == q.val);
+    }
+
+    public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
+        this.dfs(root, p, q);
+        return this.ans;
+    }
+}
+/**
+ * 给定一个二叉树, 找到该树中两个指定节点的最近公共祖先。
+ * 百度百科中最近公共祖先的定义为：“对于有根树 T 的两个节点 p、q，最近公共祖先表示为一个节点 x，满足 x 是 p、q 的祖先且 x 的深度尽可能大（一个节点也可以是它自己的祖先）。”
+ * --------------------------------------------------
+ * 提示：
+ * 树中节点数目在范围 [2, 10^5] 内。
+ * -10^9 <= Node.val <= 10^9
+ * 所有 Node.val 互不相同 。
+ * p != q
+ * p 和 q 均存在于给定的二叉树中。
+ */

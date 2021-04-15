083e34b98c79cde96b80ac0ebac141cefad783f9
Silence <72530910+Silence-Zoe@users.noreply.github.com>
2021-04-15 17:03:22 +0800

    Update 2021.02.01 LC234.回文链表（官方）.java

--- 2021.02.01 LC234.回文链表（官方）.java
+++ 2021.02.01 LC234.回文链表（官方）.java
@@ -1,3 +1,7 @@
+/**
+ * 执行用时：10 ms，在所有 Java 提交中击败了 25.53% 的用户
+ * 内存消耗：50.9 MB，在所有 Java 提交中击败了 17.95% 的用户
+ */
 class Solution {
     public boolean isPalindrome(ListNode head) {
         List<Integer> vals = new ArrayList<Integer>();
@@ -18,3 +22,6 @@ class Solution {
         return true;
     }
 }
+/**
+ * 请判断一个链表是否为回文链表。
+ */

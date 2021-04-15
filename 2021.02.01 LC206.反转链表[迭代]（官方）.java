43e02224b3b7e89f468d512aa676e0c06bf21ca7
Silence <72530910+Silence-Zoe@users.noreply.github.com>
2021-04-15 16:58:29 +0800

    Update 2021.02.01 LC206.反转链表[迭代]（官方）.java

--- 2021.02.01 LC206.反转链表[迭代]（官方）.java
+++ 2021.02.01 LC206.反转链表[迭代]（官方）.java
@@ -1,3 +1,7 @@
+/**
+ * 执行用时：0 ms，在所有 Java 提交中击败了 100.00% 的用户
+ * 内存消耗：38.3 MB，在所有 Java 提交中击败了 58.09% 的用户
+ */
 class Solution {
     public ListNode reverseList(ListNode head) {
         ListNode prev = null;
@@ -11,3 +15,6 @@ class Solution {
         return prev;
     }
 }
+/**
+ * 反转一个单链表。
+ */

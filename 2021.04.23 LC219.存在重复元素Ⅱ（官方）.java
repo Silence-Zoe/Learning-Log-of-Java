3a80d47a22ff00566cb62d9abaf9406b1d0a7f47
Silence <72530910+Silence-Zoe@users.noreply.github.com>
2021-04-23 20:36:40 +0800

    Create 2021.04.23 LC219.存在重复元素Ⅱ（官方）.java

---
+++ 2021.04.23 LC219.存在重复元素Ⅱ（官方）.java
@@ -0,0 +1,22 @@
+/**
+ * 执行用时：10 ms，在所有 Java 提交中击败了 59.30% 的用户
+ * 内存消耗：41.9 MB，在所有 Java 提交中击败了 53.63% 的用户
+ */
+class Solution {
+    public boolean containsNearbyDuplicate(int[] nums, int k) {
+        Set<Integer> set = new HashSet<>();
+        for (int i = 0; i < nums.length; ++i) {
+            if (set.contains(nums[i])) {
+                return true;
+            }
+            set.add(nums[i]);
+            if (set.size() > k) {
+                set.remove(nums[i - k]);
+            }
+        }
+        return false;
+    }
+}
+/**
+ * 给定一个整数数组和一个整数 k，判断数组中是否存在两个不同的索引 i 和 j，使得 nums [i] = nums [j]，并且 i 和 j 的差的 绝对值 至多为 k。
+ */

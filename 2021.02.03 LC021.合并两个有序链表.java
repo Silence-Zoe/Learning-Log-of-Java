23c85ed10243ea23f50d8eda40b69a5fb2bb0b1f
Silence <72530910+Silence-Zoe@users.noreply.github.com>
2021-04-26 13:02:21 +0800

    Update 2021.02.03 LC021.合并两个有序链表.java

--- 2021.02.03 LC021.合并两个有序链表.java
+++ 2021.02.03 LC021.合并两个有序链表.java
@@ -1,12 +1,6 @@
 /**
- * Definition for singly-linked list.
- * public class ListNode {
- *     int val;
- *     ListNode next;
- *     ListNode() {}
- *     ListNode(int val) { this.val = val; }
- *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
- * }
+ * 执行用时：0 ms，在所有 Java 提交中击败了 100.00% 的用户
+ * 内存消耗：37.7 MB，在所有 Java 提交中击败了 77.77% 的用户
  */
 class Solution {
     public ListNode mergeTwoLists(ListNode l1, ListNode l2) {
@@ -29,3 +23,11 @@ class Solution {
         return head;
     }
 }
+/**
+ * 将两个升序链表合并为一个新的 升序 链表并返回。新链表是通过拼接给定的两个链表的所有节点组成的。
+ * --------------------------------------------------
+ * 提示：
+ * 两个链表的节点数目范围是 [0, 50]
+ * -100 <= Node.val <= 100
+ * l1 和 l2 均按 非递减顺序 排列
+ */

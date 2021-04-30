e594773d125bd71fb6c67b2b077b868e2672e4c0
Silence <72530910+Silence-Zoe@users.noreply.github.com>
2021-04-30 21:38:50 +0800

    Update 2021.02.03 LC021.合并两个有序链表[递归]（官方）.java

--- 2021.02.03 LC021.合并两个有序链表[递归]（官方）.java
+++ 2021.02.03 LC021.合并两个有序链表[递归]（官方）.java
@@ -1,19 +1,27 @@
+/**
+ * 执行用时：0 ms，在所有 Java 提交中击败了 100.00% 的用户
+ * 内存消耗：37.9 MB，在所有 Java 提交中击败了 51.83% 的用户
+ */
 class Solution {
-    private ListNode frontPointer;
-    private boolean recursivelyCheck(ListNode currentNode) {
-        if (currentNode != null) {
-            if (!recursivelyCheck(currentNode.next)) {
-                return false;
-            }
-            if (currentNode.val != frontPointer.val) {
-                return false;
-            }
-            frontPointer = frontPointer.next;
+    public ListNode mergeTwoLists(ListNode l1, ListNode l2) {
+        if (l1 == null) {
+            return l2;
+        } else if (l2 == null) {
+            return l1;
+        } else if (l1.val < l2.val) {
+            l1.next = mergeTwoLists(l1.next, l2);
+            return l1;
+        } else {
+            l2.next = mergeTwoLists(l1, l2.next);
+            return l2;
         }
-        return true;
-    }
-    public boolean isPalindrome(ListNode head) {
-        frontPointer = head;
-        return recursivelyCheck(head);
     }
 }
+/**
+ * 将两个升序链表合并为一个新的 升序 链表并返回。新链表是通过拼接给定的两个链表的所有节点组成的。
+ * --------------------------------------------------
+ * 提示：
+ * 两个链表的节点数目范围是 [0, 50]
+ * -100 <= Node.val <= 100
+ * l1 和 l2 均按 非递减顺序 排列
+ */

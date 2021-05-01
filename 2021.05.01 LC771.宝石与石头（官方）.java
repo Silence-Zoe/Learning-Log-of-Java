43aa843915da34722c0b36f0949634c95200c67b
Silence <72530910+Silence-Zoe@users.noreply.github.com>
2021-05-01 16:46:16 +0800

    Create 2021.05.01 LC771.宝石与石头（官方）.java

---
+++ 2021.05.01 LC771.宝石与石头（官方）.java
@@ -0,0 +1,30 @@
+/**
+ * 执行用时：2 ms，在所有 Java 提交中击败了 42.22% 的用户
+ * 内存消耗：37 MB，在所有 Java 提交中击败了 28.84% 的用户
+ */
+class Solution {
+    public int numJewelsInStones(String jewels, String stones) {
+        int jewelsCount = 0;
+        Set<Character> jewelsSet = new HashSet<Character>();
+        int jewelsLength = jewels.length(), stonesLength = stones.length();
+        for (int i = 0; i < jewelsLength; i++) {
+            char jewel = jewels.charAt(i);
+            jewelsSet.add(jewel);
+        }
+        for (int i = 0; i < stonesLength; i++) {
+            char stone = stones.charAt(i);
+            if (jewelsSet.contains(stone)) {
+                jewelsCount++;
+            }
+        }
+        return jewelsCount;
+    }
+}
+/**
+ * 给定字符串J 代表石头中宝石的类型，和字符串 S代表你拥有的石头。 S 中每个字符代表了一种你拥有的石头的类型，你想知道你拥有的石头中有多少是宝石。
+ * J 中的字母不重复，J 和 S中的所有字符都是字母。字母区分大小写，因此"a"和"A"是不同类型的石头。
+ * --------------------------------------------------
+ * 提示：
+ * S 和 J 最多含有50个字母。
+ * J 中的字符不重复。
+ */

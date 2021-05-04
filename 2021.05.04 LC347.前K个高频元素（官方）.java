94ba5504b544d4f24cb078a84830287527d5ab5d
Silence <72530910+Silence-Zoe@users.noreply.github.com>
2021-05-04 10:58:01 +0800

    Create 2021.05.04 LC347.前K个高频元素（官方）.java

---
+++ 2021.05.04 LC347.前K个高频元素（官方）.java
@@ -0,0 +1,42 @@
+/**
+ * 执行用时：16 ms，在所有 Java 提交中击败了 54.25% 的用户
+ * 内存消耗：40.7 MB，在所有 Java 提交中击败了 97.17% 的用户
+ */
+class Solution {
+    public int[] topKFrequent(int[] nums, int k) {
+        Map<Integer, Integer> occurrences = new HashMap<Integer, Integer>();
+        for (int num : nums) {
+            occurrences.put(num, occurrences.getOrDefault(num, 0) + 1);
+        }
+
+        PriorityQueue<int[]> queue = new PriorityQueue<int[]>(new Comparator<int[]>() {
+            public int compare(int[] m, int[] n) {
+                return m[1] - n[1];
+            }
+        });
+        for (Map.Entry<Integer, Integer> entry : occurrences.entrySet()) {
+            int num = entry.getKey(), count = entry.getValue();
+            if (queue.size() == k) {
+                if (queue.peek()[1] < count) {
+                    queue.poll();
+                    queue.offer(new int[]{num, count});
+                }
+            } else {
+                queue.offer(new int[]{num, count});
+            }
+        }
+        int[] ret = new int[k];
+        for (int i = 0; i < k; ++i) {
+            ret[i] = queue.poll()[0];
+        }
+        return ret;
+    }
+}
+/**
+ * 给你一个整数数组 nums 和一个整数 k ，请你返回其中出现频率前 k 高的元素。你可以按 任意顺序 返回答案。
+ * --------------------------------------------------
+ * 提示：
+ * 1 <= nums.length <= 10^5
+ * k 的取值范围是 [1, 数组中不相同的元素的个数]
+ * 题目数据保证答案唯一，换句话说，数组中前 k 个高频元素的集合是唯一的
+ */

be516ce547565e10022a094afd5de0466e664b48
Silence <72530910+Silence-Zoe@users.noreply.github.com>
2021-05-10 20:27:13 +0800

    Update 2021.02.04 LC2.两数相加.java

--- 2021.02.04 LC2.两数相加.java
+++ 2021.02.04 LC2.两数相加.java
@@ -1,12 +1,6 @@
 /**
- * Definition for singly-linked list.
- * public class ListNode {
- *     int val;
- *     ListNode next;
- *     ListNode() {}
- *     ListNode(int val) { this.val = val; }
- *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
- * }
+ * 执行用时：2 ms，在所有 Java 提交中击败了 100.00% 的用户
+ * 内存消耗：38.9 MB，在所有 Java 提交中击败了 51.00% 的用户
  */
 class Solution {
     public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
@@ -42,3 +36,13 @@ class Solution {
         return ans.next;
     }
 }
+/**
+ * 给你两个 非空 的链表，表示两个非负的整数。它们每位数字都是按照 逆序 的方式存储的，并且每个节点只能存储 一位 数字。
+ * 请你将两个数相加，并以相同形式返回一个表示和的链表。
+ * 你可以假设除了数字 0 之外，这两个数都不会以 0 开头。
+ * --------------------------------------------------
+ * 提示：
+ * 每个链表中的节点数在范围 [1, 100] 内
+ * 0 <= Node.val <= 9
+ * 题目数据保证列表表示的数字不含前导零
+ */

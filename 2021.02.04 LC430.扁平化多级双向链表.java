290dab0a2130250402817733be6b0465759a3e95
Silence <72530910+Silence-Zoe@users.noreply.github.com>
2021-05-11 16:05:06 +0800

    Update 2021.02.04 LC430.扁平化多级双向链表.java

--- 2021.02.04 LC430.扁平化多级双向链表.java
+++ 2021.02.04 LC430.扁平化多级双向链表.java
@@ -1,13 +1,7 @@
-/*
-// Definition for a Node.
-class Node {
-    public int val;
-    public Node prev;
-    public Node next;
-    public Node child;
-};
-*/
-
+/**
+ * 执行用时：0 ms，在所有 Java 提交中击败了 100.00% 的用户
+ * 内存消耗：36.5 MB，在所有 Java 提交中击败了 52.47% 的用户
+ */
 class Solution {
     Node n = new Node();
     Node pn = n;
@@ -43,3 +37,11 @@ class Solution {
         return head;
     }
 }
+/**
+ * 多级双向链表中，除了指向下一个节点和前一个节点指针之外，它还有一个子链表指针，可能指向单独的双向链表。这些子列表也可能会有一个或多个自己的子项，依此类推，生成多级数据结构，如下面的示例所示。
+ * 给你位于列表第一级的头节点，请你扁平化列表，使所有结点出现在单级双链表中。
+ * --------------------------------------------------
+ * 提示：
+ * 每节点数目不超过 1000
+ * 1 <= Node.val <= 10^5
+ */

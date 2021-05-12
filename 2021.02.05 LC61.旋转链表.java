688c15891c488d31dc2df5411c975e07a919a7da
Silence <72530910+Silence-Zoe@users.noreply.github.com>
2021-05-12 16:45:04 +0800

    Update 2021.02.05 LC61.旋转链表.java

--- 2021.02.05 LC61.旋转链表.java
+++ 2021.02.05 LC61.旋转链表.java
@@ -1,12 +1,6 @@
 /**
- * Definition for singly-linked list.
- * public class ListNode {
- *     int val;
- *     ListNode next;
- *     ListNode() {}
- *     ListNode(int val) { this.val = val; }
- *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
- * }
+ * 执行用时：1 ms，在所有 Java 提交中击败了 51.35% 的用户
+ * 内存消耗：38.1 MB，在所有 Java 提交中击败了 9.58% 的用户
  */
 class Solution {
     public ListNode rotateRight(ListNode head, int k) {
@@ -34,3 +28,11 @@ class Solution {
         return ans;
     }
 }
+/**
+ * 给你一个链表的头节点 head ，旋转链表，将链表每个节点向右移动 k 个位置。
+ * --------------------------------------------------
+ * 提示：
+ * 链表中节点的数目在范围 [0, 500] 内
+ * -100 <= Node.val <= 100
+ * 0 <= k <= 2 * 10^9
+ */

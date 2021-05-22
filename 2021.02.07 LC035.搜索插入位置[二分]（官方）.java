e331cff482db430e4ac667e0305507dbf3f4369b
Silence <72530910+Silence-Zoe@users.noreply.github.com>
2021-05-22 15:26:49 +0800

    Update 2021.02.07 LC035.搜索插入位置[二分]（官方）.java

--- 2021.02.07 LC035.搜索插入位置[二分]（官方）.java
+++ 2021.02.07 LC035.搜索插入位置[二分]（官方）.java
@@ -1,3 +1,7 @@
+/**
+ * 执行用时：0 ms，在所有 Java 提交中击败了 100.00% 的用户
+ * 内存消耗：38 MB，在所有 Java 提交中击败了 68.40% 的用户
+ */
 class Solution {
     public int searchInsert(int[] nums, int target) {
         int n = nums.length;
@@ -14,3 +18,7 @@ class Solution {
         return ans;
     }
 }
+/**
+ * 给定一个排序数组和一个目标值，在数组中找到目标值，并返回其索引。如果目标值不存在于数组中，返回它将会被按顺序插入的位置。
+ * 你可以假设数组中无重复元素。
+ */

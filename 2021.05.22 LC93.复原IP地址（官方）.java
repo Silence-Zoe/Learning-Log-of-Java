7ea8bbbdb0602d20206dedd985b0f79bd1369444
Silence <72530910+Silence-Zoe@users.noreply.github.com>
2021-05-23 20:51:22 +0800

    Create 2021.05.22 LC93.复原IP地址（官方）.java

---
+++ 2021.05.22 LC93.复原IP地址（官方）.java
@@ -0,0 +1,59 @@
+/**
+ * 执行用时：1 ms，在所有 Java 提交中击败了 99.94% 的用户
+ * 内存消耗：36.9 MB，在所有 Java 提交中击败了 95.25% 的用户
+ */
+class Solution {
+    static final int SEG_COUNT = 4;
+    List<String> ans = new ArrayList<String>();
+    int[] segments = new int[SEG_COUNT];
+
+    public List<String> restoreIpAddresses(String s) {
+        segments = new int[SEG_COUNT];
+        dfs(s, 0, 0);
+        return ans;
+    }
+
+    public void dfs(String s, int segId, int segStart) {
+        if (segId == SEG_COUNT) {
+            if (segStart == s.length()) {
+                StringBuffer ipAddr = new StringBuffer();
+                for (int i = 0; i < SEG_COUNT; ++i) {
+                    ipAddr.append(segments[i]);
+                    if (i != SEG_COUNT - 1) {
+                        ipAddr.append('.');
+                    }
+                }
+                ans.add(ipAddr.toString());
+            }
+            return;
+        }
+
+        if (segStart == s.length()) {
+            return;
+        }
+
+        if (s.charAt(segStart) == '0') {
+            segments[segId] = 0;
+            dfs(s, segId + 1, segStart + 1);
+        }
+
+        int addr = 0;
+        for (int segEnd = segStart; segEnd < s.length(); ++segEnd) {
+            addr = addr * 10 + (s.charAt(segEnd) - '0');
+            if (addr > 0 && addr <= 0xFF) {
+                segments[segId] = addr;
+                dfs(s, segId + 1, segEnd + 1);
+            } else {
+                break;
+            }
+        }
+    }
+}
+/**
+ * 给你一个字符串 s，请你将 s 分割成一些子串，使每个子串都是 回文串 。返回 s 所有可能的分割方案。
+ * 回文串 是正着读和反着读都一样的字符串。
+ * --------------------------------------------------
+ * 提示：
+ * 1 <= s.length <= 16
+ * s 仅由小写英文字母组成
+ */

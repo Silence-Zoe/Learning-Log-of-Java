b27e2776c5b31d72617f77c07afba6aca0d79161
Silence <72530910+Silence-Zoe@users.noreply.github.com>
2021-06-02 13:22:39 +0800

    Create 2021.06.02 LC52.N皇后Ⅱ（官方）.java

---
+++ 2021.06.02 LC52.N皇后Ⅱ（官方）.java
@@ -0,0 +1,49 @@
+/**
+ * 执行用时：4 ms，在所有 Java 提交中击败了 35.84% 的用户
+ * 内存消耗：37.5 MB，在所有 Java 提交中击败了 13.61% 的用户
+ */
+class Solution {
+    public int totalNQueens(int n) {
+        Set<Integer> columns = new HashSet<Integer>();
+        Set<Integer> diagonals1 = new HashSet<Integer>();
+        Set<Integer> diagonals2 = new HashSet<Integer>();
+        return backtrack(n, 0, columns, diagonals1, diagonals2);
+    }
+
+    public int backtrack(int n, int row, Set<Integer> columns, Set<Integer> diagonals1, Set<Integer> diagonals2) {
+        if (row == n) {
+            return 1;
+        } else {
+            int count = 0;
+            for (int i = 0; i < n; i++) {
+                if (columns.contains(i)) {
+                    continue;
+                }
+                int diagonal1 = row - i;
+                if (diagonals1.contains(diagonal1)) {
+                    continue;
+                }
+                int diagonal2 = row + i;
+                if (diagonals2.contains(diagonal2)) {
+                    continue;
+                }
+                columns.add(i);
+                diagonals1.add(diagonal1);
+                diagonals2.add(diagonal2);
+                count += backtrack(n, row + 1, columns, diagonals1, diagonals2);
+                columns.remove(i);
+                diagonals1.remove(diagonal1);
+                diagonals2.remove(diagonal2);
+            }
+            return count;
+        }
+    }
+}
+/**
+ * n 皇后问题 研究的是如何将 n 个皇后放置在 n×n 的棋盘上，并且使皇后彼此之间不能相互攻击。
+ * 给你一个整数 n ，返回 n 皇后问题 不同的解决方案的数量。
+ * --------------------------------------------------
+ * 提示：
+ * 1 <= n <= 9
+ * 皇后彼此不能相互攻击，也就是说：任何两个皇后都不能处于同一条横行、纵行或斜线上。
+ */

77cd1730db80bc4ea352a9dff95f7f1fc41d3803
Silence <72530910+Silence-Zoe@users.noreply.github.com>
2021-06-12 14:44:10 +0800

    Create 2021.06.12 LC199.二叉树的右视图（官方）.java

---
+++ 2021.06.12 LC199.二叉树的右视图（官方）.java
@@ -0,0 +1,45 @@
+/**
+ * 执行用时：2 ms，在所有 Java 提交中击败了 23.35% 的用户
+ * 内存消耗：38.5 MB，在所有 Java 提交中击败了 6.81% 的用户
+ */
+class Solution {
+    public List<Integer> rightSideView(TreeNode root) {
+        Map<Integer, Integer> rightmostValueAtDepth = new HashMap<Integer, Integer>();
+        int max_depth = -1;
+
+        Queue<TreeNode> nodeQueue = new LinkedList<TreeNode>();
+        Queue<Integer> depthQueue = new LinkedList<Integer>();
+        nodeQueue.add(root);
+        depthQueue.add(0);
+
+        while (!nodeQueue.isEmpty()) {
+            TreeNode node = nodeQueue.remove();
+            int depth = depthQueue.remove();
+
+            if (node != null) {
+            	max_depth = Math.max(max_depth, depth);
+
+                rightmostValueAtDepth.put(depth, node.val);
+
+                nodeQueue.add(node.left);
+                nodeQueue.add(node.right);
+                depthQueue.add(depth + 1);
+                depthQueue.add(depth + 1);
+            }
+        }
+
+        List<Integer> rightView = new ArrayList<Integer>();
+        for (int depth = 0; depth <= max_depth; depth++) {
+            rightView.add(rightmostValueAtDepth.get(depth));
+        }
+
+        return rightView;
+    }
+}
+/**
+ * 给定一棵二叉树，想象自己站在它的右侧，按照从顶部到底部的顺序，返回从右侧所能看到的节点值。
+ * --------------------------------------------------
+ * 提示：
+ * 两棵树上的节点数目都在范围  [0, 100] 内
+ * -100 <= Node.val <= 100
+*/

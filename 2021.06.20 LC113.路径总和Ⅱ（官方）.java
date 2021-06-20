834c207ebfe3c46c5dbc7908fa1d4371ed623f95
Silence <72530910+Silence-Zoe@users.noreply.github.com>
2021-06-20 11:21:46 +0800

    Create 2021.06.20 LC113.路径总和Ⅱ（官方）.java

---
+++ 2021.06.20 LC113.路径总和Ⅱ（官方）.java
@@ -0,0 +1,35 @@
+/**
+ * 执行用时：1 ms，在所有 Java 提交中击败了 100.00% 的用户
+ * 内存消耗：38.9 MB，在所有 Java 提交中击败了 29.17% 的用户
+ */
+class Solution {
+    List<List<Integer>> ret = new LinkedList<List<Integer>>();
+    Deque<Integer> path = new LinkedList<Integer>();
+
+    public List<List<Integer>> pathSum(TreeNode root, int targetSum) {
+        dfs(root, targetSum);
+        return ret;
+    }
+
+    public void dfs(TreeNode root, int targetSum) {
+        if (root == null) {
+            return;
+        }
+        path.offerLast(root.val);
+        targetSum -= root.val;
+        if (root.left == null && root.right == null && targetSum == 0) {
+            ret.add(new LinkedList<Integer>(path));
+        }
+        dfs(root.left, targetSum);
+        dfs(root.right, targetSum);
+        path.pollLast();
+    }
+}
+/**
+ * 给你二叉树的根节点 root 和一个整数目标和 targetSum ，找出所有 从根节点到叶子节点 路径总和等于给定目标和的路径。
+ * --------------------------------------------------
+ * 提示：
+ * 树中节点总数在范围 [0, 5000] 内
+ * -1000 <= Node.val <= 1000
+ * -1000 <= targetSum <= 1000
+ */

eed0de15e48719c5dde0ed0d2e89c9c21888400a
Silence <72530910+Silence-Zoe@users.noreply.github.com>
2021-06-26 13:44:06 +0800

    Create 2021.06.26 LC235.二叉搜索树的最近公共祖先（官方）.java

---
+++ 2021.06.26 LC235.二叉搜索树的最近公共祖先（官方）.java
@@ -0,0 +1,29 @@
+/**
+ * 执行用时：6 ms，在所有 Java 提交中击败了 100.00% 的用户
+ * 内存消耗：39.4 MB，在所有 Java 提交中击败了 37.33% 的用户
+ */
+class Solution {
+    public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
+        TreeNode ancestor = root;
+        while (true) {
+            if (p.val < ancestor.val && q.val < ancestor.val) {
+                ancestor = ancestor.left;
+            } else if (p.val > ancestor.val && q.val > ancestor.val) {
+                ancestor = ancestor.right;
+            } else {
+                break;
+            }
+        }
+        return ancestor;
+    }
+}
+/**
+ * 给定一个二叉搜索树, 找到该树中两个指定节点的最近公共祖先。
+ * 百度百科中最近公共祖先的定义为：“对于有根树 T 的两个结点 p、q，最近公共祖先表示为一个结点 x，满足 x 是 p、q 的祖先且 x 的深度尽可能大（一个节点也可以是它自己的祖先）。”
+ * --------------------------------------------------
+ * 提示：
+ * 树中节点总数在范围 [2, 10^5] 内
+ * -10^9 <= Node.val <= 10^9
+ * 所有节点的值都是唯一的。
+ * p、q 为不同节点且均存在于给定的二叉搜索树中。
+ */

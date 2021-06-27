88b7f5df2d3eeb7ab9a031c57ff04bc4353c6ca4
Silence <72530910+Silence-Zoe@users.noreply.github.com>
2021-06-27 15:24:57 +0800

    Update 2021.02.08 LC014.最长公共前缀（官方）.java

--- 2021.02.08 LC014.最长公共前缀（官方）.java
+++ 2021.02.08 LC014.最长公共前缀（官方）.java
@@ -1,3 +1,7 @@
+/**
+ * 执行用时：1 ms，在所有 Java 提交中击败了 84.26% 的用户
+ * 内存消耗：36.4 MB，在所有 Java 提交中击败了 72.79% 的用户
+ */
 class Solution {
     public String longestCommonPrefix(String[] strs) {
         if (strs == null || strs.length == 0) {
@@ -16,3 +20,12 @@ class Solution {
         return strs[0];
     }
 }
+/**
+ * 编写一个函数来查找字符串数组中的最长公共前缀。
+ * 如果不存在公共前缀，返回空字符串 ""。
+ * --------------------------------------------------
+ * 提示：
+ * 0 <= strs.length <= 200
+ * 0 <= strs[i].length <= 200
+ * strs[i] 仅由小写英文字母组成
+ */

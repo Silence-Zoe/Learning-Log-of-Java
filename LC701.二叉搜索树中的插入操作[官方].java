f841884246a38480ae2c0f88a2067d74a93682b6
Silence <72530910+Silence-Zoe@users.noreply.github.com>
2021-06-30 15:55:52 +0800

    Create LC701.二叉搜索树中的插入操作[官方].java

---
+++ LC701.二叉搜索树中的插入操作[官方].java
@@ -0,0 +1,40 @@
+/**
+ * 执行用时：0 ms，在所有 Java 提交中击败了 100.00% 的用户
+ * 内存消耗：39 MB，在所有 Java 提交中击败了 75.70% 的用户
+ */
+class Solution {
+    public TreeNode insertIntoBST(TreeNode root, int val) {
+        if (root == null) {
+            return new TreeNode(val);
+        }
+        TreeNode pos = root;
+        while (pos != null) {
+            if (val < pos.val) {
+                if (pos.left == null) {
+                    pos.left = new TreeNode(val);
+                    break;
+                } else {
+                    pos = pos.left;
+                }
+            } else {
+                if (pos.right == null) {
+                    pos.right = new TreeNode(val);
+                    break;
+                } else {
+                    pos = pos.right;
+                }
+            }
+        }
+        return root;
+    }
+}
+/**
+ * 给定二叉搜索树（BST）的根节点和要插入树中的值，将值插入二叉搜索树。 返回插入后二叉搜索树的根节点。 输入数据 保证 ，新值和原始二叉搜索树中的任意节点值都不同。
+ * 注意，可能存在多种有效的插入方式，只要树在插入后仍保持为二叉搜索树即可。 你可以返回 任意有效的结果 。
+ * --------------------------------------------------
+ * 提示：
+ * 给定的树上的节点数介于 0 和 10^4 之间
+ * 每个节点都有一个唯一整数值，取值范围从 0 到 10^8
+ * -10^8 <= val <= 10^8
+ * 新值和原始二叉搜索树中的任意节点值都不同
+ */

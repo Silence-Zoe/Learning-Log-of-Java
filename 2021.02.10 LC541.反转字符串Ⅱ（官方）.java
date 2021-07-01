6416bb3c36282d5bcf6509aef68089c02522c963
Silence <72530910+Silence-Zoe@users.noreply.github.com>
2021-07-01 15:20:41 +0800

    Update 2021.02.10 LC541.反转字符串Ⅱ（官方）.java

--- 2021.02.10 LC541.反转字符串Ⅱ（官方）.java
+++ 2021.02.10 LC541.反转字符串Ⅱ（官方）.java
@@ -1,7 +1,11 @@
+/**
+ * 执行用时：2 ms，在所有 Java 提交中击败了 34.06% 的用户
+ * 内存消耗：38.5 MB，在所有 Java 提交中击败了 52.42% 的用户
+ */
 class Solution {
     public String reverseStr(String s, int k) {
         char[] a = s.toCharArray();
-        for (int start = 0; s  < a.length; start += 2 * k) {
+        for (int start = 0; start < a.length; start += 2 * k) {
             int i = start, j = Math.min(start + k - 1, a.length - 1);
             while (i < j) {
                 char tmp = a[i];
@@ -12,3 +16,12 @@ class Solution {
         return new String(a);
     }
 }
+/**
+ * 给定一个字符串 s 和一个整数 k，你需要对从字符串开头算起的每隔 2k 个字符的前 k 个字符进行反转。
+ *     如果剩余字符少于 k 个，则将剩余字符全部反转。
+ *     如果剩余字符小于 2k 但大于或等于 k 个，则反转前 k 个字符，其余字符保持原样。
+ * --------------------------------------------------
+ * 提示：
+ * 该字符串只包含小写英文字母。
+ * 给定字符串的长度和 k 在 [1, 10000] 范围内。
+ */

dbe422ea9f4ab2d29a53e13a5d0d5d4c58a5105e
Silence <72530910+Silence-Zoe@users.noreply.github.com>
2021-07-05 11:33:24 +0800

    Create LC669.修剪二叉搜索树（官方）.java

---
+++ LC669.修剪二叉搜索树（官方）.java
@@ -0,0 +1,27 @@
+/**
+ * 执行用时：0 ms，在所有 Java 提交中击败了 100.00% 的用户
+ * 内存消耗：38 MB，在所有 Java 提交中击败了 60.42% 的用户
+ */
+class Solution {
+    public TreeNode trimBST(TreeNode root, int L, int R) {
+        if (root == null) return root;
+        if (root.val > R) return trimBST(root.left, L, R);
+        if (root.val < L) return trimBST(root.right, L, R);
+
+        root.left = trimBST(root.left, L, R);
+        root.right = trimBST(root.right, L, R);
+        return root;
+    }
+}
+/**
+ *
+ * 给你二叉搜索树的根节点 root ，同时给定最小边界low 和最大边界 high。通过修剪二叉搜索树，使得所有节点的值在[low, high]中。修剪树不应该改变保留在树中的元素的相对结构（即，如果没有被移除，原有的父代子代关系都应当保留）。 可以证明，存在唯一的答案。
+ * 所以结果应当返回修剪好的二叉搜索树的新的根节点。注意，根节点可能会根据给定的边界发生改变。
+ * --------------------------------------------------
+ * 提示：
+ * 树中节点数在范围 [1, 10^4] 内
+ * 0 <= Node.val <= 10^4
+ * 树中每个节点的值都是唯一的
+ * 题目数据保证输入是一棵有效的二叉搜索树
+ * 0 <= low <= high <= 10^4
+ */

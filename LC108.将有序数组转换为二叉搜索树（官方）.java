2d9b45599e487e8ff38f36789fac3a4552ddd078
Silence <72530910+Silence-Zoe@users.noreply.github.com>
2021-07-06 09:48:14 +0800

    Create LC108.将有序数组转换为二叉搜索树（官方）.java

---
+++ LC108.将有序数组转换为二叉搜索树（官方）.java
@@ -0,0 +1,31 @@
+/**
+ * 执行用时：0 ms，在所有 Java 提交中击败了 100.00% 的用户
+ * 内存消耗：38.3 MB，在所有 Java 提交中击败了 26.71% 的用户
+ */
+class Solution {
+    public TreeNode sortedArrayToBST(int[] nums) {
+        return helper(nums, 0, nums.length - 1);
+    }
+
+    public TreeNode helper(int[] nums, int left, int right) {
+        if (left > right) {
+            return null;
+        }
+
+        int mid = (left + right) / 2;
+
+        TreeNode root = new TreeNode(nums[mid]);
+        root.left = helper(nums, left, mid - 1);
+        root.right = helper(nums, mid + 1, right);
+        return root;
+    }
+}
+/**
+ * 给你一个整数数组 nums ，其中元素已经按 升序 排列，请你将其转换为一棵 高度平衡 二叉搜索树。
+ * 高度平衡 二叉树是一棵满足「每个节点的左右两个子树的高度差的绝对值不超过 1 」的二叉树。
+ * --------------------------------------------------
+ * 提示：
+ * 1 <= nums.length <= 10^4
+ * -10^4 <= nums[i] <= 10^4
+ * nums 按 严格递增 顺序排列
+ */

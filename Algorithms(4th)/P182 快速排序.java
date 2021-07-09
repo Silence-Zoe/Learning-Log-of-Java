f10fb74402996c3904c41a2bba5086eb82b8ee58
Silence <72530910+Silence-Zoe@users.noreply.github.com>
2021-07-09 23:06:01 +0800

    Create P182 快速排序.java

---
+++ Algorithms(4th)/P182 快速排序.java
@@ -0,0 +1,50 @@
+class Quick {
+    public static void sort(Comparable[] a) {
+        shuffle(a);
+        sort(a, 0, a.length - 1);
+    }
+
+    private static void sort(Comparable[] a, int lo, int hi) {
+        if (hi <= lo) {
+            return;
+        }
+        int j = partition(a, lo, hi);
+        sort(a, lo, j - 1);
+        sort(a, j + 1, hi);
+    }
+
+    private static void shuffle(Comparable[] a) {
+        Random rand = new Random();
+        int length = a.length;
+        for (int i = length; i > 0; i--) {
+            int randInd = rand.nextInt(i);
+            exch(a, randInd, i - 1);
+        }
+    }
+
+    private static boolean less(Comparable v, Comparable w) {
+        return v.compareTo(w) < 0;
+    }
+
+    private static void exch(Comparable[] a, int i, int j) {
+        Comparable t = a[i];
+        a[i] = a[j];
+        a[j] = t;
+    }
+
+    private static void show(Comparable[] a) {
+        for (int i = 0; i < a.length; i++) {
+            System.out.print(a[i] + " ");
+        }
+        System.out.println();
+    }
+
+    public static boolean isSorted(Comparable[] a) {
+        for (int i = 1; i < a.length; i++) {
+            if (less(a[i], a[i - 1])) {
+                return false;
+            }
+        }
+        return true;
+    }
+}

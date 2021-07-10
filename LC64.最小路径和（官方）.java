3c190a2bbde0f13b8c45a617d7aeffa3b6787dc3
Silence <72530910+Silence-Zoe@users.noreply.github.com>
2021-07-10 21:07:35 +0800

    Create LC64.最小路径和（官方）.java

---
+++ LC64.最小路径和（官方）.java
@@ -0,0 +1,40 @@
+/**
+ * 执行用时：2 ms，在所有 Java 提交中击败了 98.07% 的用户
+ * 内存消耗：40.9 MB，在所有 Java 提交中击败了 83.20% 的用户
+ */
+class Solution {
+    public int minPathSum(int[][] grid) {
+        if (grid == null || grid.length == 0 || grid[0].length == 0) {
+            return 0;
+        }
+        
+        int rows = grid.length, columns = grid[0].length;
+        int[][] dp = new int[rows][columns];
+        dp[0][0] = grid[0][0];
+        
+        for (int i = 1; i < rows; i++) {
+            dp[i][0] = dp[i - 1][0] + grid[i][0];
+        }
+        
+        for (int j = 1; j < columns; j++) {
+            dp[0][j] = dp[0][j - 1] + grid[0][j];
+        }
+        
+        for (int i = 1; i < rows; i++) {
+            for (int j = 1; j < columns; j++) {
+                dp[i][j] = Math.min(dp[i - 1][j], dp[i][j - 1]) + grid[i][j];
+            }
+        }
+        
+        return dp[rows - 1][columns - 1];
+    }
+}
+/**
+ * 给定一个包含非负整数的 m x n 网格 grid ，请找出一条从左上角到右下角的路径，使得路径上的数字总和为最小。
+ * --------------------------------------------------
+ * 提示：
+ * m == grid.length
+ * n == grid[i].length
+ * 1 <= m, n <= 200
+ * 0 <= grid[i][j] <= 100
+ */

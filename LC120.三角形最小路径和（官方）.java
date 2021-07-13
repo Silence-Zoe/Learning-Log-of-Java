f9136add7ba42d65e03d60a6a7f34b0d83fe2333
Silence <72530910+Silence-Zoe@users.noreply.github.com>
2021-07-13 10:17:19 +0800

    Create LC120.三角形最小路径和（官方）.java

---
+++ LC120.三角形最小路径和（官方）.java
@@ -0,0 +1,35 @@
+/**
+ * 执行用时：2 ms，在所有 Java 提交中击败了 95.83% 的用户
+ * 内存消耗：38.5 MB，在所有 Java 提交中击败了 55.12% 的用户
+ */
+class Solution {
+    public int minimumTotal(List<List<Integer>> triangle) {
+        int n = triangle.size();
+        int[][] f = new int[2][n];
+        f[0][0] = triangle.get(0).get(0);
+        for (int i = 1; i < n; ++i) {
+            int curr = i % 2;
+            int prev = 1 - curr;
+            f[curr][0] = f[prev][0] + triangle.get(i).get(0);
+            for (int j = 1; j < i; ++j) {
+                f[curr][j] = Math.min(f[prev][j - 1], f[prev][j]) + triangle.get(i).get(j);
+            }
+            f[curr][i] = f[prev][i - 1] + triangle.get(i).get(i);
+        }
+        int minTotal = f[(n - 1) % 2][0];
+        for (int i = 1; i < n; ++i) {
+            minTotal = Math.min(minTotal, f[(n - 1) % 2][i]);
+        }
+        return minTotal;
+    }
+}
+/**
+ * 给定一个三角形 triangle ，找出自顶向下的最小路径和。
+ * 每一步只能移动到下一行中相邻的结点上。相邻的结点 在这里指的是 下标 与 上一层结点下标 相同或者等于 上一层结点下标 + 1 的两个结点。也就是说，如果正位于当前行的下标 i ，那么下一步可以移动到下一行的下标 i 或 i + 1 。
+ * --------------------------------------------------
+ * 提示：
+ * 1 <= triangle.length <= 200
+ * triangle[0].length == 1
+ * triangle[i].length == triangle[i - 1].length + 1
+ * -10^4 <= triangle[i][j] <= 10^4
+ */
